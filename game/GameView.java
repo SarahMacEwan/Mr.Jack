b3f5a4edcde26e936315ebc557fe3b6acd1e11e4
Sarah MacEwan <smmacewan@mta.ca>
2018-10-31 22:18:35 -0300

    Mouse clicks work on individual rects

--- game/GameView.java
+++ game/GameView.java
@@ -23,33 +23,29 @@ import javafx.scene.control.Button;
 
 public class GameView extends Application{
 
-
-
-
 	//instance variables go here :D
+	//includes list of buttons we need, and so on
+	private Rectangle[] row1;
+	private Rectangle[] row2;
+	private Rectangle[] row3;
+	private Rectangle[] row4;
+	private Rectangle[] row5;
 
 	public GameView(){}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		/*
-	This is mostly me(Sarah) messing with figuring out how to draw hexagons in JavaFx
-	Based on an example from TutorialsPoint for now :D
-	 */
+		//add the initializations of the buttons and so on for the view
 
 		Group root = new Group();
 
-		//create a new rectangle object
-		Rectangle myRect = new Rectangle(10, 10, 50, 50);
-		myRect.setStroke(Color.BLACK);
-		myRect.setFill(Color.GREY);
-		myRect.setStrokeWidth(3);
+		row1 = drawEvenRow(10, 10, root);
+		row2 = drawOddRow(60, 110, root);
+		row3 = drawEvenRow(10, 210, root);
+		row4 = drawOddRow(60, 310, root);
+		row5 = drawEvenRow(10, 410, root);
 
-		drawEvenRow(10, 10, root);
-		drawOddRow(60, 110, root);
-		drawEvenRow(10, 210, root);
-		drawOddRow(60, 310, root);
-		drawEvenRow(10, 410, root);
+		handle();
 
 		Scene scene = new Scene(root, 600, 300);
 		primaryStage.setTitle("This is a test Hexagon");
@@ -80,6 +76,95 @@ public class GameView extends Application{
 			group.getChildren().add(newRect);
 		}
 	}
+
+	private void clicksInRow1(){
+		//loop for registering clicks in row1
+		for(int x = 0; x < 10; x++){
+			Rectangle rect = row1[x];
+			rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					//
+					System.out.println("This works??!?!?!");
+				}
+			});
+		}
+	}
+
+	private void clicksInRow2(){
+		//loop for registering clicks in row2
+		for(int x = 0; x < 9; x++){
+			Rectangle rect = row2[x];
+			rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					//
+					System.out.println("This works??!?!?!");
+				}
+			});
+		}
+	}
+
+	private void clicksInRow3(){
+		//loop for registering clicks in row3
+		for(int x = 0; x < 10; x++){
+			Rectangle rect = row3[x];
+			rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					//
+					System.out.println("This works??!?!?!");
+				}
+			});
+		}
+	}
+
+	private void clicksInRow4(){
+		//loop for registering clicks in row4
+		for(int x = 0; x < 9; x++){
+			Rectangle rect = row4[x];
+			rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					//
+					System.out.println("This works??!?!?!");
+				}
+			});
+		}
+	}
+
+	private void clicksInRow5(){
+		//loop for registering clicks in row5
+		for(int x = 0; x < 10; x++){
+			Rectangle rect = row5[x];
+			rect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					//
+					System.out.println("This works??!?!?!");
+				}
+			});
+		}
+	}
+
+
+	private void handle(){
+		//method for handling all possible user action events :D
+		//it's messy, but it works, friends :D
+
+		clicksInRow1();
+
+		clicksInRow2();
+
+		clicksInRow3();
+
+		clicksInRow4();
+
+		clicksInRow5();
+
+
+	}
+
 
 	public void update(String everything) {
 
