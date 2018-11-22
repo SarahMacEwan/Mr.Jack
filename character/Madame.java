3592c30f7608268fb32a4e607ab570ce5c2f64f7
Sarah MacEwan <smmacewan@mta.ca>
2018-11-21 22:56:20 -0400

    Making all the files be the same :)

--- character/Madame.java
+++ character/Madame.java
@@ -1,21 +1,69 @@
 package character;
 
+import java.util.ArrayList;
+import game.Board;
+import tile.Exit;
 import game.GameModel;
 import tile.Tile;
 
 /**
- * Madame can move up to 6 tiles and cannot go through the manholes
+ * When moving, Madame can never use the sewers
+ *
  */
-public class Madame extends MrJackCharacter{
+public class Madame extends MrJackCharacter {
+
+//---  Constant Values   ----------------------------------------------------------------------
+
+    /** Constant String object representing the name of this InspectorLestrade object*/
+    private static final String NAME = "Madame";
+    /** Constant int value representing the total number of moves this InspectorLestrade object can do in one turn*/
+    private static final int NUM_MOVES = 6;
+    /** Constant String object representing the shorthand name for this MrJackCharacter object*/
+    private static final String SHORT_NAME = "Mad.";
+
+// --- Instance Variables ---------------------------------------------------------------------
+//---  Constructors   -------------------------------------------------------------------------
+
+    /**
+     * Constructor for objects of the MissStealthy class, assigning the constant values for the
+     * object's name and number of moves.
+     */
+
+    public Madame() {
+        name = NAME;
+        numMoves = NUM_MOVES;
+        shortName = SHORT_NAME;
+    }
+
+//---  Operations   ---------------------------------------------------------------------------
+
+    public boolean canMove(Tile tile, int dist) {
+        Tile[] tempChoice = new Tile[1];
+        tempChoice[0] = tile;
+        if((this.ability(tempChoice))&& dist <= numMoves){
+            return true;
+        } else return false;
+    }
+
+    /* The way we played/interpreted the game was that you could not stand on a manhole without
+     * using it this means that Madame is not allowed to stand on any manholes.
+    */
     @Override
     public boolean ability(Tile[] choice) {
-        return false;
+        for(Tile tile: choice) {
+            if (tile.getIdentity() == 'm') {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void deriveFromModel(GameModel model) {
 
     }
+
+//---  Ability Queries   ----------------------------------------------------------------------
 
     @Override
     public int requiredValuesForAbility() {
@@ -24,7 +72,7 @@ public class Madame extends MrJackCharacter{
 
     @Override
     public boolean hasToDoAbility() {
-        return false;
+        return true;
     }
 
     @Override
@@ -39,11 +87,11 @@ public class Madame extends MrJackCharacter{
 
     @Override
     public boolean canDoAbilityAfter() {
-        return false;
+        return true;
     }
 
     @Override
-    public int getNUMMOVES(){
+    public int getNUMMOVES() {
         return NUM_MOVES;
     }
 }
--- game/GameController.java
+++ game/GameController.java
@@ -2,6 +2,7 @@ package game;
 
 import character.*;
 import java.io.*;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
@@ -15,30 +16,42 @@ import javax.swing.JFrame;
 public class GameController {
 	
 //---  Constant Values   ----------------------------------------------------------------------
-	//TODO: make 2 arrays, one to initialize basic characters, and one to initialze optional characters (2 of each)
-	private static final MrJackCharacter[] CHARACTERS = {new InspectorLestrade(),
-														 new JohnSmith(),
-														 new MissStealthy(),
-														 new SirWilliamGull()};
-    private static final int SCREEN_WIDTH = 1222;
-    private static final int SCREEN_HEIGHT = 854;
-	
+
+	private static final int NUM_CHARACTERS = 4;
+	private static final MrJackCharacter[] CHARACTERS = {};
+	/** MrJackCharacter[] object representing all of the viable MrJackCharacters that can be used in this game*/
+	private static final MrJackCharacter[] BASIC_CHARACTERS = {new JohnSmith(), new InspectorLestrade()};
+	/** MrJackCharacter[] object representing all of the viable MrJackCharacters that can be used in this game*/
+	private static final MrJackCharacter[] OPTIONAL_CHARACTERS = {new MissStealthy(), new SirWilliamGull(), new Madame(), new InspectorAbberline()};
+	private static final int SCREEN_WIDTH = 1222;
+	private static final int SCREEN_HEIGHT = 854;
+
 //---  Instance Variables   -------------------------------------------------------------------
-	
+
 	/** */
 	GameModel theGame;
 	/** */
 	GameView theView;
 
 //---  Constructors   -------------------------------------------------------------------------
-	
-    public GameController(File f){
-    	JFrame frame = readyFrame();
-        theGame = new GameModel(f, CHARACTERS);
-        theView = new GameView(this);
-        frame.add(theView);
-    }
-    
+
+	public GameController(File f){
+
+		JFrame frame = readyFrame();
+		Random rm = new Random();
+		for (int i = 0; i < NUM_CHARACTERS/2; i++) {
+			int temp = rm.nextInt(4);
+			CHARACTERS[i] = OPTIONAL_CHARACTERS[temp];
+		}
+		for (int i = 2; i < NUM_CHARACTERS/2; i++) {
+			int temp = rm.nextInt(2);
+			CHARACTERS[i] =BASIC_CHARACTERS[temp];
+		}
+		theGame = new GameModel(f, CHARACTERS);
+		theView = new GameView(this);
+		frame.add(theView);
+		// Just initialize CHARACTERS array to have two of each
+	}
 //---  Operations   ---------------------------------------------------------------------------
 
     public void startMrJack() {
